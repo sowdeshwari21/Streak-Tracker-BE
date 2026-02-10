786363dd6c0fb638f1cb9b1c0f21f15de959aa9c
Sowdesh21 <sowdesh2003@gmail.com>
2026-02-10 18:18:39 +0530

    cors added

--- src/main/java/intern/example/demo/config/CorsConfig.java
+++ src/main/java/intern/example/demo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500/")
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500/","https://strick-tracker-pro.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
